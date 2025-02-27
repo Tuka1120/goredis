dc3738b396344316ed329a7b4e7e6002f7f8a16c
2kman <tulgaaaaa1120@gmail.com>
2025-02-27 18:03:02 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"goredis/client"
+	"log"
+	"log/slog"
+	"net"
+	"time"
+)
+
+const defaultListenAddr = ":5001"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	peers     map[*Peer]bool
+	ln        net.Listener
+	addPeerch chan *Peer
+	quitCh    chan struct{}
+	msgCh     chan []byte
+
+	//
+	kv *KV
+}
+
+func NewServer(cfg Config) *Server {
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+	return &Server{
+		Config:    cfg,
+		peers:     make(map[*Peer]bool),
+		addPeerch: make(chan *Peer),
+		quitCh:    make(chan struct{}),
+		msgCh:     make(chan []byte),
+		kv:        NewKV(),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	go s.loop()
+
+	slog.Info("Server is running", "listenAddr", s.ListenAddr)
+
+	return s.acceptLoop()
+}
+
+func (s *Server) handleRawMessage(rawMsg []byte) error {
+	cmd, err := parseCommand(string(rawMsg))
+	if err != nil {
+		return err
+	}
+	switch v := cmd.(type) {
+	case SetCommand:
+		return s.kv.Set(v.key, v.val)
+	}
+
+	return nil
+}
+
+func (s *Server) loop() {
+	for {
+		select {
+		case rawMsg := <-s.msgCh:
+			if err := s.handleRawMessage(rawMsg); err != nil {
+				slog.Error("Raw message error", "err", err)
+			}
+		case <-s.quitCh:
+			return
+		case peer := <-s.addPeerch:
+			s.peers[peer] = true
+		}
+	}
+}
+
+func (s *Server) acceptLoop() error {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			slog.Error("accept error", "err", err)
+			continue
+		}
+		go s.handleConn(conn)
+	}
+}
+
+func (s *Server) handleConn(conn net.Conn) {
+	peer := NewPeer(conn, s.msgCh)
+	s.addPeerch <- peer
+	slog.Info("new peer connected", "remoteAddr", conn.RemoteAddr())
+	if err := peer.readLoop(); err != nil {
+		slog.Error("peer read error", "err", err, "remoteAddr", conn.RemoteAddr())
+	}
+}
+
+func main() {
+	server := NewServer(Config{})
+	go func() {
+		log.Fatal(server.Start())
+	}()
+	time.Sleep(time.Second)
+
+	for i := 0; i < 10; i++ {
+		c := client.New("localhost:5001")
+		if err := c.Set(context.TODO(), fmt.Sprintf("foo_%d", i), fmt.Sprintf("bar_%d", i)); err != nil {
+			log.Fatal(err)
+		}
+	}
+	time.Sleep(time.Second)
+	fmt.Println(server.kv.data)
+}
